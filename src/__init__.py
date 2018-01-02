4945c4ae1b4b4b3003b0889961d19cbbc42cea1b
zd-zero <zaerald.zd@gmail.com>
2018-01-03 01:22:29 +0800

    Fix Word Search on Available Letters

--- src/__init__.py
+++ src/__init__.py
@@ -2,19 +2,36 @@
 # Wordscapes Cheat - to view all possible answers by entering all available letters
 
 import json
-import os
-import sys
 import time
+from itertools import combinations
 from itertools import permutations
 
 
+def load_words():
+    try:
+        with open('word_dictionary.json', 'r') as dictionary:
+            return json.load(dictionary)
+    except Exception as e:
+        return str(e)
+
+
 print('Enter available letters: ', end='')
+english_words = load_words()
 available_letters = list(input())
 start_time = time.time()
 
-for word in list(permutations(available_letters)):
-    word = ''.join(word)
-    print(word)
 
+for i in range(3, len(available_letters) + 1):
+    print('=' * 20)
+    print(f'Word Length {i}:')
+    word_list = []
+    for word_combination in list(combinations(available_letters, i)):
+        for word in list(permutations(word_combination)):
+            word = ''.join(word)
+            if english_words.get(word):
+                word_list.append(word)
+    word_list.sort()
+    for word in word_list:
+        print(word)
 
 print(f'Program Finished at {time.time() - start_time:.4f}s')
