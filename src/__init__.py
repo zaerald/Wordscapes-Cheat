3523d878135677c714352d3eb240e9b15946c949
zd-zero <zaerald.zd@gmail.com>
2018-01-03 00:46:28 +0800

    Add Permutations on Available Letters

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,20 @@
 #! python3
 # Wordscapes Cheat - to view all possible answers by entering all available letters
 
+import json
+import os
+import sys
+import time
+from itertools import permutations
+
+
+print('Enter available letters: ', end='')
+available_letters = list(input())
+start_time = time.time()
+
+for word in list(permutations(available_letters)):
+    word = ''.join(word)
+    print(word)
+
+
+print(f'Program Finished at {time.time() - start_time:.4f}s')
